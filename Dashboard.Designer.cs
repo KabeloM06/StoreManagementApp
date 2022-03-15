baf95f39e944bad7944467923c28da367e4835b9
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-03-15 10:36:24 +0200

    Create connection to SQL database

--- Dashboard.Designer.cs
+++ Dashboard.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.btnPlaceOrder = new System.Windows.Forms.Button();
-            this.btnAddItems = new System.Windows.Forms.Button();
-            this.btnRemoveItems = new System.Windows.Forms.Button();
-            this.btnEditItems = new System.Windows.Forms.Button();
-            this.btnExit = new System.Windows.Forms.Button();
             this.lblLogOut = new System.Windows.Forms.LinkLabel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnEditItems = new System.Windows.Forms.Button();
+            this.btnRemoveItems = new System.Windows.Forms.Button();
+            this.btnAddItems = new System.Windows.Forms.Button();
+            this.btnPlaceOrder = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.welcome1 = new StoreManagementApp.UserControls.Welcome();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -55,62 +55,19 @@
             this.panel1.Size = new System.Drawing.Size(200, 520);
             this.panel1.TabIndex = 0;
             // 
-            // panel2
+            // lblLogOut
             // 
-            this.panel2.BackColor = System.Drawing.SystemColors.Info;
-            this.panel2.Controls.Add(this.welcome1);
-            this.panel2.Location = new System.Drawing.Point(210, 12);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(755, 520);
-            this.panel2.TabIndex = 0;
-            // 
-            // btnPlaceOrder
-            // 
-            this.btnPlaceOrder.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.btnPlaceOrder.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnPlaceOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPlaceOrder.Location = new System.Drawing.Point(39, 51);
-            this.btnPlaceOrder.Name = "btnPlaceOrder";
-            this.btnPlaceOrder.Size = new System.Drawing.Size(175, 52);
-            this.btnPlaceOrder.TabIndex = 0;
-            this.btnPlaceOrder.Text = "Place Order";
-            this.btnPlaceOrder.UseVisualStyleBackColor = false;
-            // 
-            // btnAddItems
-            // 
-            this.btnAddItems.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.btnAddItems.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnAddItems.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddItems.Location = new System.Drawing.Point(39, 126);
-            this.btnAddItems.Name = "btnAddItems";
-            this.btnAddItems.Size = new System.Drawing.Size(175, 52);
-            this.btnAddItems.TabIndex = 1;
-            this.btnAddItems.Text = "Add Items";
-            this.btnAddItems.UseVisualStyleBackColor = false;
-            // 
-            // btnRemoveItems
-            // 
-            this.btnRemoveItems.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.btnRemoveItems.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnRemoveItems.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRemoveItems.Location = new System.Drawing.Point(39, 277);
-            this.btnRemoveItems.Name = "btnRemoveItems";
-            this.btnRemoveItems.Size = new System.Drawing.Size(175, 52);
-            this.btnRemoveItems.TabIndex = 2;
-            this.btnRemoveItems.Text = "Remove Items";
-            this.btnRemoveItems.UseVisualStyleBackColor = false;
-            // 
-            // btnEditItems
-            // 
-            this.btnEditItems.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.btnEditItems.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnEditItems.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEditItems.Location = new System.Drawing.Point(39, 202);
-            this.btnEditItems.Name = "btnEditItems";
-            this.btnEditItems.Size = new System.Drawing.Size(175, 52);
-            this.btnEditItems.TabIndex = 3;
-            this.btnEditItems.Text = "Edit Items";
-            this.btnEditItems.UseVisualStyleBackColor = false;
+            this.lblLogOut.AutoSize = true;
+            this.lblLogOut.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.lblLogOut.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogOut.LinkColor = System.Drawing.Color.Black;
+            this.lblLogOut.Location = new System.Drawing.Point(64, 470);
+            this.lblLogOut.Name = "lblLogOut";
+            this.lblLogOut.Size = new System.Drawing.Size(72, 19);
+            this.lblLogOut.TabIndex = 5;
+            this.lblLogOut.TabStop = true;
+            this.lblLogOut.Text = "Log Out";
+            this.lblLogOut.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblLogOut_LinkClicked);
             // 
             // btnExit
             // 
@@ -125,19 +82,62 @@
             this.btnExit.UseVisualStyleBackColor = false;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
-            // lblLogOut
+            // btnEditItems
             // 
-            this.lblLogOut.AutoSize = true;
-            this.lblLogOut.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.lblLogOut.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLogOut.LinkColor = System.Drawing.Color.Black;
-            this.lblLogOut.Location = new System.Drawing.Point(64, 470);
-            this.lblLogOut.Name = "lblLogOut";
-            this.lblLogOut.Size = new System.Drawing.Size(72, 19);
-            this.lblLogOut.TabIndex = 5;
-            this.lblLogOut.TabStop = true;
-            this.lblLogOut.Text = "Log Out";
-            this.lblLogOut.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblLogOut_LinkClicked);
+            this.btnEditItems.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnEditItems.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEditItems.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditItems.Location = new System.Drawing.Point(39, 202);
+            this.btnEditItems.Name = "btnEditItems";
+            this.btnEditItems.Size = new System.Drawing.Size(175, 52);
+            this.btnEditItems.TabIndex = 3;
+            this.btnEditItems.Text = "Edit Items";
+            this.btnEditItems.UseVisualStyleBackColor = false;
+            // 
+            // btnRemoveItems
+            // 
+            this.btnRemoveItems.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnRemoveItems.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRemoveItems.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemoveItems.Location = new System.Drawing.Point(39, 277);
+            this.btnRemoveItems.Name = "btnRemoveItems";
+            this.btnRemoveItems.Size = new System.Drawing.Size(175, 52);
+            this.btnRemoveItems.TabIndex = 2;
+            this.btnRemoveItems.Text = "Remove Items";
+            this.btnRemoveItems.UseVisualStyleBackColor = false;
+            // 
+            // btnAddItems
+            // 
+            this.btnAddItems.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnAddItems.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAddItems.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddItems.Location = new System.Drawing.Point(39, 126);
+            this.btnAddItems.Name = "btnAddItems";
+            this.btnAddItems.Size = new System.Drawing.Size(175, 52);
+            this.btnAddItems.TabIndex = 1;
+            this.btnAddItems.Text = "Add Items";
+            this.btnAddItems.UseVisualStyleBackColor = false;
+            // 
+            // btnPlaceOrder
+            // 
+            this.btnPlaceOrder.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnPlaceOrder.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPlaceOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlaceOrder.Location = new System.Drawing.Point(39, 51);
+            this.btnPlaceOrder.Name = "btnPlaceOrder";
+            this.btnPlaceOrder.Size = new System.Drawing.Size(175, 52);
+            this.btnPlaceOrder.TabIndex = 0;
+            this.btnPlaceOrder.Text = "Place Order";
+            this.btnPlaceOrder.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Info;
+            this.panel2.Controls.Add(this.welcome1);
+            this.panel2.Location = new System.Drawing.Point(210, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(755, 520);
+            this.panel2.TabIndex = 0;
             // 
             // welcome1
             // 
@@ -158,7 +158,6 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Dashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Dashboard";
             this.Load += new System.EventHandler(this.Dashboard_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
---
+++ function.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StoreManagementApp
+{
+    internal class function
+    {
+        // Connect to database
+        protected SqlConnection getConnection()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString= "data source = KABELOLAPTOP\\SQLEXPRESS;" +
+                "database = StoreManagementApp; " +
+                "integrated security = True;";
+
+            return conn;
+        }
+
+        // Fetch data
+        public DataSet getData(String query)
+        {
+            SqlConnection conn = getConnection();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = query;
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+
+        // set data or process data
+
+        public void setData(String query)
+        {
+            SqlConnection conn = getConnection();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection= conn;
+            conn.Open();
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            MessageBox.Show("Data Processed Successfully.", "success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
